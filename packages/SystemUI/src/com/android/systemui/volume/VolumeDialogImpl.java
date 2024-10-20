LMODroid/platform_frameworks_base

VolumeDialog: Fix per-app volume on secondary users
Change-Id: I3ee1767d8482948f4c5d885b4eb8f0c259cfa4b3

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -82,6 +82,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.SystemClock;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.provider.Settings;
 import android.provider.Settings.Global;
@@ -1428,7 +1429,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private boolean shouldShowAppVolume() {
         ContentResolver cr = mContext.getContentResolver();
-        int showAppVolume = Settings.System.getInt(cr, LMOSettings.System.SHOW_APP_VOLUME, 0);
+        int showAppVolume = Settings.System.getIntForUser(cr, LMOSettings.System.SHOW_APP_VOLUME,
+                0, UserHandle.USER_CURRENT);
         boolean ret = showAppVolume == 1;
         if (ret) {
             ret = false;

franklin-albuquerque/frameworks_base

packages/SystemUI/src/com/android/systemui/volume/VolumeDialogImpl.java
@@ -75,6 +75,7 @@ import android.os.Looper;
 import android.os.Message;
 import android.os.SystemClock;
 import android.os.Trace;
+import android.os.UserHandle;
 import android.os.VibrationEffect;
 import android.provider.Settings;
 import android.provider.Settings.Global;
@@ -1342,7 +1343,8 @@ public class VolumeDialogImpl implements VolumeDialog, Dumpable,
 
     private boolean shouldShowAppVolume() {
         ContentResolver cr = mContext.getContentResolver();
-        int showAppVolume = Settings.System.getInt(cr, Settings.System.SHOW_APP_VOLUME, 0);
+        int showAppVolume = Settings.System.getIntForUser(cr, Settings.System.SHOW_APP_VOLUME,
+                0, UserHandle.USER_CURRENT);
         if (showAppVolume == 1) {
             AudioManager audioManager = mController.getAudioManager();
             for (AppVolume av : audioManager.listAppVolumes()) {