Legendleo90/havoc_fwb

SystemUI: Improve network traffic layout and add back unit support
Signed-off-by: Anushek Prasal <anushekprasal@gmail.com>

packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -43,12 +43,13 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
             "lineagesecure:" + LineageSettings.Secure.NETWORK_TRAFFIC_MODE;
     private static final String NETWORK_TRAFFIC_AUTOHIDE =
             "lineagesecure:" + LineageSettings.Secure.NETWORK_TRAFFIC_AUTOHIDE;
+    private static final String NETWORK_TRAFFIC_UNIT_TYPE =
+            "lineagesecure:" + LineageSettings.Secure.NETWORK_TRAFFIC_UNITS;
 
     private static final int INTERVAL = 1500; //ms
     private static final int KB = 1024;
     private static final int MB = KB * KB;
     private static final int GB = MB * KB;
-    private static final String symbol = "/S";
 
     private final int mWidth;
 
@@ -58,6 +59,7 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
     private long totalTxBytes;
     private long lastUpdateTime;
     private int mAutoHideThreshold;
+    private int mUnitType;
     protected int mTintColor;
 
     private boolean mScreenOn = true;
@@ -93,7 +95,7 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
                 mVisible = false;
             } else if (shouldShowUpload(rxData, txData, timeDelta)) {
                 // Show information for uplink if it's called for
-                CharSequence output = formatOutput(timeDelta, txData, symbol);
+                CharSequence output = formatOutput(timeDelta, txData);
 
                 // Update view if there's anything new to show
                 if (output != getText()) {
@@ -102,7 +104,7 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
                 makeVisible();
             } else {
                 // Add information for downlink if it's called for
-                CharSequence output = formatOutput(timeDelta, rxData, symbol);
+                CharSequence output = formatOutput(timeDelta, rxData);
 
                 // Update view if there's anything new to show
                 if (output != getText()) {
@@ -118,8 +120,8 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
             mTrafficHandler.postDelayed(mRunnable, INTERVAL);
         }
 
-        private CharSequence formatOutput(long timeDelta, long data, String symbol) {
-            long speed = (long)(data / (timeDelta / 1000F));
+        private CharSequence formatOutput(long timeDelta, long data) {
+            long speed = (long) (data / (timeDelta / 1000F));
 
             return formatDecimal(speed);
         }
@@ -130,57 +132,70 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
             String formatSpeed;
             SpannableString spanUnitString;
             SpannableString spanSpeedString;
+            String gunit, munit, kunit;
 
-            if (speed >= 1000 * MB) {
-                unit = "GB";
-                decimalFormat = new DecimalFormat("0.00");
-                formatSpeed =  decimalFormat.format(speed / (float)GB);
-            } else if (speed >= 100 * MB) {
-                decimalFormat = new DecimalFormat("000");
-                unit = "MB";
-                formatSpeed =  decimalFormat.format(speed / (float)MB);
-            } else if (speed >= 10 * MB) {
-                decimalFormat = new DecimalFormat("00.0");
-                unit = "MB";
-                formatSpeed =  decimalFormat.format(speed / (float)MB);
-            } else if (speed >= 1000 * KB) {
-                decimalFormat = new DecimalFormat("0.00");
-                unit = "MB";
-                formatSpeed =  decimalFormat.format(speed / (float)MB);
-            } else if (speed >= 100 * KB) {
-                decimalFormat = new DecimalFormat("000");
-                unit = "KB";
-                formatSpeed =  decimalFormat.format(speed / (float)KB);
-            } else if (speed >= 10 * KB) {
-                decimalFormat = new DecimalFormat("00.0");
-                unit = "KB";
-                formatSpeed =  decimalFormat.format(speed / (float)KB);
+            if (mUnitType == 0) {
+                gunit = mContext.getString(R.string.gigabytespersecond_short);
+                munit = mContext.getString(R.string.megabytespersecond_short);
+                kunit = mContext.getString(R.string.kilobytespersecond_short);
             } else {
-                decimalFormat = new DecimalFormat("0.00");
-                unit = "KB";
-                formatSpeed = decimalFormat.format(speed / (float)KB);
+                // speed is in bytes, convert to bits
+                speed = speed * 8;
+                gunit = mContext.getString(R.string.gigabitspersecond_short);
+                munit = mContext.getString(R.string.megabitspersecond_short);
+                kunit = mContext.getString(R.string.kilobitspersecond_short);
+            }
+
+            if (speed >= GB) {
+                unit = gunit;
+                decimalFormat = new DecimalFormat("0.##");
+                formatSpeed = decimalFormat.format(speed / (float) GB);
+            } else if (speed >= 100 * MB) {
+                decimalFormat = new DecimalFormat("##0");
+                unit = munit;
+                formatSpeed = decimalFormat.format(speed / (float) MB);
+            } else if (speed >= 10 * MB) {
+                decimalFormat = new DecimalFormat("#0.#");
+                unit = munit;
+                formatSpeed = decimalFormat.format(speed / (float) MB);
+            } else if (speed >= MB) {
+                decimalFormat = new DecimalFormat("0.##");
+                unit = munit;
+                formatSpeed = decimalFormat.format(speed / (float) MB);
+            } else if (speed >= 100 * KB) {
+                decimalFormat = new DecimalFormat("##0");
+                unit = kunit;
+                formatSpeed = decimalFormat.format(speed / (float) KB);
+            } else if (speed >= 10 * KB) {
+                decimalFormat = new DecimalFormat("#0.#");
+                unit = kunit;
+                formatSpeed = decimalFormat.format(speed / (float) KB);
+            } else {
+                decimalFormat = new DecimalFormat("0.##");
+                unit = kunit;
+                formatSpeed = decimalFormat.format(speed / (float) KB);
             }
             spanSpeedString = new SpannableString(formatSpeed);
             spanSpeedString.setSpan(getSpeedRelativeSizeSpan(), 0, (formatSpeed).length(),
                     Spanned.SPAN_INCLUSIVE_INCLUSIVE);
 
-            spanUnitString = new SpannableString(unit + symbol);
-            spanUnitString.setSpan(getUnitRelativeSizeSpan(), 0, (unit + symbol).length(),
+            spanUnitString = new SpannableString(unit);
+            spanUnitString.setSpan(getUnitRelativeSizeSpan(), 0, (unit).length(),
                     Spanned.SPAN_INCLUSIVE_INCLUSIVE);
             return TextUtils.concat(spanSpeedString, "\n", spanUnitString);
         }
 
         private boolean shouldHide(long rxData, long txData, long timeDelta) {
-            long speedRxKB = (long)(rxData / (timeDelta / 1000f)) / KB;
-            long speedTxKB = (long)(txData / (timeDelta / 1000f)) / KB;
+            long speedRxKB = (long) (rxData / (timeDelta / 1000f)) / KB;
+            long speedTxKB = (long) (txData / (timeDelta / 1000f)) / KB;
             return !getConnectAvailable() ||
                     (speedRxKB < mAutoHideThreshold &&
-                    speedTxKB < mAutoHideThreshold);
+                            speedTxKB < mAutoHideThreshold);
         }
 
         private boolean shouldShowUpload(long rxData, long txData, long timeDelta) {
-            long speedRxKB = (long)(rxData / (timeDelta / 1000f)) / KB;
-            long speedTxKB = (long)(txData / (timeDelta / 1000f)) / KB;
+            long speedRxKB = (long) (rxData / (timeDelta / 1000f)) / KB;
+            long speedTxKB = (long) (txData / (timeDelta / 1000f)) / KB;
 
             return (speedTxKB > speedRxKB);
         }
@@ -232,6 +247,7 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
             final TunerService tunerService = Dependency.get(TunerService.class);
             tunerService.addTunable(this, NETWORK_TRAFFIC_MODE);
             tunerService.addTunable(this, NETWORK_TRAFFIC_AUTOHIDE);
+            tunerService.addTunable(this, NETWORK_TRAFFIC_UNIT_TYPE);
             IntentFilter filter = new IntentFilter();
             filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
             filter.addAction(Intent.ACTION_SCREEN_OFF);
@@ -251,12 +267,12 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
         }
     }
 
-    protected RelativeSizeSpan getSpeedRelativeSizeSpan() {
-        return new RelativeSizeSpan(0.78f);
+    private RelativeSizeSpan getSpeedRelativeSizeSpan() {
+        return new RelativeSizeSpan(0.70f);
     }
 
-    protected RelativeSizeSpan getUnitRelativeSizeSpan() {
-        return new RelativeSizeSpan(0.70f);
+    private RelativeSizeSpan getUnitRelativeSizeSpan() {
+        return new RelativeSizeSpan(0.65f);
     }
 
     private Runnable mRunnable = new Runnable() {
@@ -275,7 +291,12 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
                 update();
                 break;
             case NETWORK_TRAFFIC_AUTOHIDE:
-                mAutoHideThreshold = TunerService.parseIntegerSwitch(newValue, false) ? 10 : 0;
+                mAutoHideThreshold = TunerService.parseIntegerSwitch(newValue, false) ? 5 : 0;
+                setMode();
+                update();
+                break;
+            case NETWORK_TRAFFIC_UNIT_TYPE:
+                mUnitType = TunerService.parseInteger(newValue, 0);
                 setMode();
                 update();
                 break;
@@ -323,10 +344,10 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
     }
 
     protected void setMode() {
-        setGravity(Gravity.CENTER);
+        setGravity(Gravity.END|Gravity.CENTER_VERTICAL);
         setMaxLines(2);
         setSpacingAndFonts();
-        updateTrafficDrawable();
+        setTextColor(mTintColor);
         setWidth(mWidth);
     }
 
@@ -341,9 +362,10 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
         setTextColor(mTintColor);
     }
 
-    protected void setSpacingAndFonts() {
-        setTextAppearance(R.style.TextAppearance_QS_Status);
-        setLineSpacing(0.88f, 0.88f);
+    private void setSpacingAndFonts() {
+        setTypeface(Typeface.create(getResources().getString(
+                com.android.internal.R.string.config_headlineFontFamily), Typeface.BOLD));
+        setLineSpacing(0.80f, 0.80f);
     }
 
     public void onDensityOrFontScaleChanged() {

franklin-albuquerque/frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/policy/NetworkTraffic.java
@@ -45,12 +45,13 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
             "customsecure:" + Settings.Secure.NETWORK_TRAFFIC_MODE;
     private static final String NETWORK_TRAFFIC_AUTOHIDE =
             "customsecure:" + Settings.Secure.NETWORK_TRAFFIC_AUTOHIDE;
+    private static final String NETWORK_TRAFFIC_UNIT_TYPE =
+            "customsecure:" + Settings.Secure.NETWORK_TRAFFIC_UNITS;
 
     private static final int INTERVAL = 1500; //ms
     private static final int KB = 1024;
     private static final int MB = KB * KB;
     private static final int GB = MB * KB;
-    private static final String symbol = "/S";
 
     private final int mWidth;
 
@@ -60,6 +61,7 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
     private long totalTxBytes;
     private long lastUpdateTime;
     private int mAutoHideThreshold;
+    private int mUnitType;
     protected int mTintColor;
 
     private boolean mScreenOn = true;
@@ -95,7 +97,7 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
                 mVisible = false;
             } else if (shouldShowUpload(rxData, txData, timeDelta)) {
                 // Show information for uplink if it's called for
-                CharSequence output = formatOutput(timeDelta, txData, symbol);
+                CharSequence output = formatOutput(timeDelta, txData);
 
                 // Update view if there's anything new to show
                 if (output != getText()) {
@@ -104,7 +106,7 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
                 makeVisible();
             } else {
                 // Add information for downlink if it's called for
-                CharSequence output = formatOutput(timeDelta, rxData, symbol);
+                CharSequence output = formatOutput(timeDelta, rxData);
 
                 // Update view if there's anything new to show
                 if (output != getText()) {
@@ -120,8 +122,8 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
             mTrafficHandler.postDelayed(mRunnable, INTERVAL);
         }
 
-        private CharSequence formatOutput(long timeDelta, long data, String symbol) {
-            long speed = (long)(data / (timeDelta / 1000F));
+        private CharSequence formatOutput(long timeDelta, long data) {
+            long speed = (long) (data / (timeDelta / 1000F));
 
             return formatDecimal(speed);
         }
@@ -132,57 +134,70 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
             String formatSpeed;
             SpannableString spanUnitString;
             SpannableString spanSpeedString;
+            String gunit, munit, kunit;
 
-            if (speed >= 1000 * MB) {
-                unit = "GB";
-                decimalFormat = new DecimalFormat("0.00");
-                formatSpeed =  decimalFormat.format(speed / (float)GB);
-            } else if (speed >= 100 * MB) {
-                decimalFormat = new DecimalFormat("000");
-                unit = "MB";
-                formatSpeed =  decimalFormat.format(speed / (float)MB);
-            } else if (speed >= 10 * MB) {
-                decimalFormat = new DecimalFormat("00.0");
-                unit = "MB";
-                formatSpeed =  decimalFormat.format(speed / (float)MB);
-            } else if (speed >= 1000 * KB) {
-                decimalFormat = new DecimalFormat("0.00");
-                unit = "MB";
-                formatSpeed =  decimalFormat.format(speed / (float)MB);
-            } else if (speed >= 100 * KB) {
-                decimalFormat = new DecimalFormat("000");
-                unit = "KB";
-                formatSpeed =  decimalFormat.format(speed / (float)KB);
-            } else if (speed >= 10 * KB) {
-                decimalFormat = new DecimalFormat("00.0");
-                unit = "KB";
-                formatSpeed =  decimalFormat.format(speed / (float)KB);
+            if (mUnitType == 0) {
+                gunit = mContext.getString(R.string.gigabytespersecond_short);
+                munit = mContext.getString(R.string.megabytespersecond_short);
+                kunit = mContext.getString(R.string.kilobytespersecond_short);
             } else {
-                decimalFormat = new DecimalFormat("0.00");
-                unit = "KB";
-                formatSpeed = decimalFormat.format(speed / (float)KB);
+                // speed is in bytes, convert to bits
+                speed = speed * 8;
+                gunit = mContext.getString(R.string.gigabitspersecond_short);
+                munit = mContext.getString(R.string.megabitspersecond_short);
+                kunit = mContext.getString(R.string.kilobitspersecond_short);
+            }
+
+            if (speed >= GB) {
+                unit = gunit;
+                decimalFormat = new DecimalFormat("0.##");
+                formatSpeed = decimalFormat.format(speed / (float) GB);
+            } else if (speed >= 100 * MB) {
+                decimalFormat = new DecimalFormat("##0");
+                unit = munit;
+                formatSpeed = decimalFormat.format(speed / (float) MB);
+            } else if (speed >= 10 * MB) {
+                decimalFormat = new DecimalFormat("#0.#");
+                unit = munit;
+                formatSpeed = decimalFormat.format(speed / (float) MB);
+            } else if (speed >= MB) {
+                decimalFormat = new DecimalFormat("0.##");
+                unit = munit;
+                formatSpeed = decimalFormat.format(speed / (float) MB);
+            } else if (speed >= 100 * KB) {
+                decimalFormat = new DecimalFormat("##0");
+                unit = kunit;
+                formatSpeed = decimalFormat.format(speed / (float) KB);
+            } else if (speed >= 10 * KB) {
+                decimalFormat = new DecimalFormat("#0.#");
+                unit = kunit;
+                formatSpeed = decimalFormat.format(speed / (float) KB);
+            } else {
+                decimalFormat = new DecimalFormat("0.##");
+                unit = kunit;
+                formatSpeed = decimalFormat.format(speed / (float) KB);
             }
             spanSpeedString = new SpannableString(formatSpeed);
             spanSpeedString.setSpan(getSpeedRelativeSizeSpan(), 0, (formatSpeed).length(),
                     Spanned.SPAN_INCLUSIVE_INCLUSIVE);
 
-            spanUnitString = new SpannableString(unit + symbol);
-            spanUnitString.setSpan(getUnitRelativeSizeSpan(), 0, (unit + symbol).length(),
+            spanUnitString = new SpannableString(unit);
+            spanUnitString.setSpan(getUnitRelativeSizeSpan(), 0, (unit).length(),
                     Spanned.SPAN_INCLUSIVE_INCLUSIVE);
             return TextUtils.concat(spanSpeedString, "\n", spanUnitString);
         }
 
         private boolean shouldHide(long rxData, long txData, long timeDelta) {
-            long speedRxKB = (long)(rxData / (timeDelta / 1000f)) / KB;
-            long speedTxKB = (long)(txData / (timeDelta / 1000f)) / KB;
+            long speedRxKB = (long) (rxData / (timeDelta / 1000f)) / KB;
+            long speedTxKB = (long) (txData / (timeDelta / 1000f)) / KB;
             return !getConnectAvailable() ||
                     (speedRxKB < mAutoHideThreshold &&
-                    speedTxKB < mAutoHideThreshold);
+                            speedTxKB < mAutoHideThreshold);
         }
 
         private boolean shouldShowUpload(long rxData, long txData, long timeDelta) {
-            long speedRxKB = (long)(rxData / (timeDelta / 1000f)) / KB;
-            long speedTxKB = (long)(txData / (timeDelta / 1000f)) / KB;
+            long speedRxKB = (long) (rxData / (timeDelta / 1000f)) / KB;
+            long speedTxKB = (long) (txData / (timeDelta / 1000f)) / KB;
 
             return (speedTxKB > speedRxKB);
         }
@@ -234,6 +249,7 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
             final TunerService tunerService = Dependency.get(TunerService.class);
             tunerService.addTunable(this, NETWORK_TRAFFIC_MODE);
             tunerService.addTunable(this, NETWORK_TRAFFIC_AUTOHIDE);
+            tunerService.addTunable(this, NETWORK_TRAFFIC_UNIT_TYPE);
             IntentFilter filter = new IntentFilter();
             filter.addAction(ConnectivityManager.CONNECTIVITY_ACTION);
             filter.addAction(Intent.ACTION_SCREEN_OFF);
@@ -253,12 +269,12 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
         }
     }
 
-    protected RelativeSizeSpan getSpeedRelativeSizeSpan() {
-        return new RelativeSizeSpan(0.78f);
+    private RelativeSizeSpan getSpeedRelativeSizeSpan() {
+        return new RelativeSizeSpan(0.70f);
     }
 
-    protected RelativeSizeSpan getUnitRelativeSizeSpan() {
-        return new RelativeSizeSpan(0.70f);
+    private RelativeSizeSpan getUnitRelativeSizeSpan() {
+        return new RelativeSizeSpan(0.65f);
     }
 
     private Runnable mRunnable = new Runnable() {
@@ -277,7 +293,12 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
                 update();
                 break;
             case NETWORK_TRAFFIC_AUTOHIDE:
-                mAutoHideThreshold = TunerService.parseIntegerSwitch(newValue, false) ? 10 : 0;
+                mAutoHideThreshold = TunerService.parseIntegerSwitch(newValue, false) ? 5 : 0;
+                setMode();
+                update();
+                break;
+            case NETWORK_TRAFFIC_UNIT_TYPE:
+                mUnitType = TunerService.parseInteger(newValue, 0);
                 setMode();
                 update();
                 break;
@@ -325,10 +346,10 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
     }
 
     protected void setMode() {
-        setGravity(Gravity.CENTER);
+        setGravity(Gravity.END|Gravity.CENTER_VERTICAL);
         setMaxLines(2);
         setSpacingAndFonts();
-        updateTrafficDrawable();
+        setTextColor(mTintColor);
         setWidth(mWidth);
     }
 
@@ -343,9 +364,10 @@ public class NetworkTraffic extends TextView implements TunerService.Tunable {
         setTextColor(mTintColor);
     }
 
-    protected void setSpacingAndFonts() {
-        setTextAppearance(R.style.TextAppearance_QS_Status);
-        setLineSpacing(0.88f, 0.88f);
+    private void setSpacingAndFonts() {
+        setTypeface(Typeface.create(getResources().getString(
+                com.android.internal.R.string.config_headlineFontFamily), Typeface.BOLD));
+        setLineSpacing(0.80f, 0.80f);
     }
 
     public void onDensityOrFontScaleChanged() {